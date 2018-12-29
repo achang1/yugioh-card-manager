d7ba23c8daf2d5bb765d96e8b56fc00d8b40f2ce
Richard Chang <richardc123456789@gmail.com>
2018-12-29 13:30:07 -0800

    Accepts different card types

--- scraper.py
+++ scraper.py
@@ -1,63 +1,75 @@
 from requests import get
 from bs4 import BeautifulSoup
-# import pandas as pd
+import re
+### still have to fix multiple types, -> [machine/union/effect] 3 types
+#find correct URL, distinguished between monster, spell, trap
+def findurl(input):
+    if input == 1:
+            url = 'https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=1&keyword=&stype=1&ctype=1' #type 1 - monster
+    elif input == 2:
+        url = 'https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=1&keyword=&stype=1&ctype=2' #type 2 - spell
+    else:
+        url = 'https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=1&keyword=&stype=1&ctype=3' #type 3 - trap
 
-
-# request content of [NORMAL] monster card [LEVEL 1-4] [ATK 0-3000] [DEF 0-3000] [ATTR WATER] [SPECIES=TYPE SEA SERPENT]
-# sort = ?, rp = 100 [show 100 items], page=1 [first page]
-# url = 'https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=2&sort=1&rp=100&page=1'
-url = 'https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=1&keyword=&stype=1&ctype=1&starfr=1&' \
-      'starto=4&pscalefr=&pscaleto=&linkmarkerfr=&linkmarkerto=&link_m=2&atkfr=0&atkto=3000&deffr=0&defto=3000&' \
-      'attr=13&species=5&othercon=2&other=1&sort=1&rp=100&page=1'
-
+    return url
+input = 1
+#get text from url
+url = findurl(input)
 response = get(url)
 html_soup = BeautifulSoup(response.text, 'html.parser')
-
 card_containers = html_soup.find_all('dl')
 
 # List for storing scraped data
 names = []
 attributes = []
-levels = []
-types = []
-atks = []
-defs = []
+levels = [] # if spell or trap, put space
+types_0 = [] #normal,continuous, effect,etc.
+types_1 = [] #insect,beast, spell/trap = ''
+atks = [] #if spell or trap, put space
+defs = [] #if speel or trap, put space
 descriptions = []
 
 # Extract each monster card
 for card in card_containers:
+    # declare name, attribute, level, type, atk, def, description
+    name = card.find('span', class_='card_status').text.strip()
+    attribute = card.find('span', class_='box_card_attribute').text
+    description = card.find('dd', class_='box_card_text').text.strip()
 
-    # Parse name
-    name = card.strong.text
+    if input == 1:
+        level = int(card.find('span', class_='box_card_level_rank').text.strip().split(sep=' ')[1])
+        card_type_0 = card.find('span', class_='card_info_species_and_other_item').text.strip().strip('[').strip(']')\
+        .split(sep='/')[0].strip()
+        card_type_1 = card.find('span', class_='card_info_species_and_other_item').text.strip().strip('[').strip(']')\
+        .split(sep='/')[1].strip()
+        atk = int(card.find('span', class_='atk_power').text.strip().split(sep=' ')[1])
+        defence = int(card.find('span', class_='def_power').text.strip().split(sep=' ')[1])
+
+    else:
+        level =''
+        card_type_0 = card.find('span', class_='box_card_effect').text.strip()
+        card_type_1 = ''
+        atk =''
+        defence =''
+
+    #append to respective array
     names.append(name)
-
-    # Parse attribute
-    attribute = card.find('span', class_='box_card_attribute').text.strip()
+    print(name)
     attributes.append(attribute)
-
-    # Parse level/rank
-    level = int(card.find('span', class_='box_card_level_rank').text.strip().split(sep=' ')[1])
+    print(attribute)
     levels.append(level)
-
-    # Parse type
-    card_type = card.find('span', class_='card_info_species_and_other_item').text.strip().strip('[').strip(']').\
-        split(sep='/')
-    types.append(card_type)
-
-    # Parse atk points
-    atk = int(card.find('span', class_='atk_power').text.strip().split(sep=' ')[1])
+    print(level)
+    types_0.append(card_type_0)
+    print(card_type_0)
+    types_1.append(card_type_1)
+    print(card_type_1)
     atks.append(atk)
-
-    # Parse def points
-    defence = int(card.find('span', class_='def_power').text.strip().split(sep=' ')[1])
+    print(atk)
     defs.append(defence)
-
-    # Parse card effect/description
-    card_text = card.find('dd', class_='box_card_text')
-    for br in card_text.find_all('br'):
-        br.replace_with("\n")
-    description = card_text.text.strip()
+    print(defence)
     descriptions.append(description)
+    print(description)
+
 
 
 # test_df = pd.DataFrame({'name': names,
